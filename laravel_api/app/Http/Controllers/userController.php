f25ad5beb52fecaa9fb786d7603e886f966e91f2
abdul <kaiumrk5896999@gmail.com>
2022-04-06 16:41:18 +0600

    this is first commit

---
+++ laravel_api/app/Http/Controllers/userController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class userController extends Controller
+{
+    //
+    function register(Request $req){
+        $user=new User;
+        $user->uname=$req->input('name');
+        $user->uemail=$req->input('email');
+        $user->upassword=Hash::make($req->input('password'));
+        $user->save();
+        return $user;
+
+    }
+}
